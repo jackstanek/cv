8e80353ca0a576cb5798ab7f13d4acc6b1c3d4ce
Jack Stanek <jrstanek@wisc.edu>
2023-09-06 23:14:50 -0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "CV";
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-23.05;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    with flake-utils.lib; eachSystem allSystems (system:
+    let
+      docname = "cv";
+      pkgs = nixpkgs.legacyPackages.${system};
+      tex = pkgs.texlive.combine {
+          inherit (pkgs.texlive) scheme-minimal latex-bin latexmk;
+      };
+    in rec {
+      packages = {
+        document = pkgs.stdenvNoCC.mkDerivation rec {
+          name = "cv";
+          src = self;
+          buildInputs = [ pkgs.coreutils tex ];
+          phases = ["unpackPhase" "buildPhase" "installPhase"];
+          buildPhase = ''
+            export PATH="${pkgs.lib.makeBinPath buildInputs}";
+            mkdir -p .cache/texmf-var
+            env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+              latexmk -interaction=nonstopmode -pdf -lualatex \
+              ${docname}.tex
+          '';
+          installPhase = ''
+            mkdir -p $out
+            cp ${docname}.pdf $out/
+          '';
+        };
+      };
+      defaultPackage = packages.document;
+    });
+}
