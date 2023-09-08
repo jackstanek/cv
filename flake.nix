90f0cb772f45a32c0d8ef65a5ef90225d61eb4fb
Jack Stanek <jrstanek@wisc.edu>
2023-09-08 11:55:46 -0500

    Update build to use "just" in devshell

--- flake.nix
+++ flake.nix
@@ -5,44 +5,49 @@
     flake-utils.url = github:numtide/flake-utils;
   };
   outputs = { self, nixpkgs, flake-utils }:
-    with flake-utils.lib; eachSystem allSystems (system:
-    let
-      docname = "cv";
-      pkgs = nixpkgs.legacyPackages.${system};
-      tex = pkgs.texlive.combine {
-        inherit
-          (pkgs.texlive)
-          scheme-small
-          academicons
-          arydshln
-          fontawesome5
-          latex-bin
-          latexmk
-          marvosym
-          moderncv
-          multirow
-        ;
-      };
-    in rec {
-      packages = {
-        document = pkgs.stdenvNoCC.mkDerivation rec {
-          name = "cv";
-          src = self;
-          buildInputs = [ pkgs.coreutils tex ];
-          phases = ["unpackPhase" "buildPhase" "installPhase"];
-          buildPhase = ''
-            export PATH="${pkgs.lib.makeBinPath buildInputs}";
-            mkdir -p .cache/texmf-var
-            env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
-              latexmk -interaction=nonstopmode -pdf -lualatex \
-              ${docname}.tex
-          '';
-          installPhase = ''
-            mkdir -p $out
-            cp ${docname}.pdf $out/
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        docname = "cv";
+        pkgs = nixpkgs.legacyPackages.${system};
+        tex = pkgs.texlive.combine {
+          inherit
+            (pkgs.texlive)
+            scheme-small
+            academicons
+            arydshln
+            fontawesome5
+            latex-bin
+            latexmk
+            marvosym
+            moderncv
+            multirow
+          ;
+        };
+        deps = with pkgs; [ tex coreutils just bash ];
+      in rec {
+        devShells.default = pkgs.mkShell {
+          buildInputs = deps;
+          shellHook = ''
+            export DOCNAME=${docname}
           '';
         };
-      };
-      defaultPackage = packages.document;
-    });
+        packages = {
+          document = pkgs.stdenvNoCC.mkDerivation rec {
+            name = "cv";
+            src = self;
+            buildInputs = deps;
+            phases = ["unpackPhase" "buildPhase" "installPhase"];
+            buildPhase = ''
+              export DOCNAME="${docname}"
+              export PATH="${pkgs.lib.makeBinPath buildInputs}"
+              just build
+            '';
+            installPhase = ''
+              mkdir -p $out
+              cp ${docname}.pdf $out/
+            '';
+          };
+        };
+        packages.default = packages.document;
+      });
 }
